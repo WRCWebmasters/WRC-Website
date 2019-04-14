7db1a51b3578330554ffe28bba8c3869de6d91ee
caiismyname <davidcai2012@gmail.com>
2019-04-14 00:11:32 -0500

    Updated some pages, script is cleaner and can support multiple values for an entry

--- scripts/populate-names.js
+++ scripts/populate-names.js
@@ -4,8 +4,15 @@ var jsonPath = "./people.json";
 $.getJSON(jsonPath, function( data ) {
     $.each( data, function( key, val ) {
         people[key] = val;
+        if (val['value'] instanceof Array) {
+            people[key]['value'] = val['value'].join(" & ");
+        }
     });
 
-    document.body.innerHTML = document.body.innerHTML.replace(/@coordinator/g, people['coordinator']['value']);
-    document.body.innerHTML = document.body.innerHTML.replace(/@email-coordinator/g, people['coordinator']['email']);
+    for (const [ key, value ] of Object.entries(people)) {
+        var regex = new RegExp(value['tag'], 'g');
+        console.log(value['tag'], value['value']);
+
+        document.body.innerHTML = document.body.innerHTML.replace(regex, value['value']);
+    }
 });
