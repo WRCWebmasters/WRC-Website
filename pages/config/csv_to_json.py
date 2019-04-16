562e0f11fd9a3b8a48a00c60edaef6e1c9ed5071
Erica Zhong <ez8@rice.edu>
2019-04-15 19:38:58 -0500

    fellows csv populating

--- pages/config/csv_to_json.py
+++ pages/config/csv_to_json.py
@@ -5,6 +5,7 @@ import sys
 
 BASIC_ENTRY_JSON_NAME = "people.json"
 PAA_JSON_NAME = "paa.json"
+FELLOWS_JSON_NAME = "fellows.json"
 
 class BasicEntry:
     def __init__(self, role, tag, email, names):
@@ -74,6 +75,42 @@ def generatePaaEntries(inputFile):
     
     print("Created {} with {} entries".format(PAA_JSON_NAME, len(list(finalDict.keys()))))
 
+class FellowEntry:
+    def __init__(self, name, email, major, classes, category):
+        self.name = name
+        self.email = email if len(email) > 0 else None
+        self.classes = classes
+        self.major = major
+        self.category = category
+
+def generateFellowEntries(inputFile):
+    allEntries = []
+
+    # CSV columns are: [name] [email] [major] [classes]
+    with open(inputFile) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0: 
+                if (row[0] != "Name" or row[1] != "Email" or row[2] != "Major" or row[3] != "Classes" or row[4] != "Category"):
+                    print("ERROR: " + inputFile + " column names are not in format \"Name, Email, Major, Classes, Category\"")
+                    return
+            else:
+                allEntries.append(FellowEntry(row[0], row[1], row[2], row[3], row[4]))
+            
+            line_count += 1
+
+    # Export all entries to dictionary 
+    finalDict = {}
+    for entry in allEntries:
+        val = {'email': entry.email, 'major': entry.major, 'classes': entry.classes, 'category': entry.category}
+        finalDict[entry.name] = val
+
+    with open(FELLOWS_JSON_NAME, 'w') as fp:
+        json.dump(finalDict, fp)
+    
+    print("Created {} with {} entries".format(FELLOWS_JSON_NAME, len(list(finalDict.keys()))))
+
 
 
 if len(sys.argv) == 1 or "-h" in sys.argv:
@@ -88,3 +125,9 @@ else:
     if "-p" in sys.argv:
         # name of file must follow 
         generatePaaEntries(sys.argv[1 + sys.argv.index("-p")])
+
+    if "-f" in sys.argv:
+        # name of file must follow
+        generateFellowEntries(sys.argv[1 + sys.argv.index("-f")])
+
+
---
+++ scripts/populate-fellows.js
@@ -0,0 +1,40 @@
+var jsonPath = "./config/fellows.json";
+
+$.getJSON(jsonPath, function( data ) {
+    $.each( data, function( key, value ) {
+        var tr = document.createElement("tr");
+        var td_name = document.createElement("td");
+        td_name.innerHTML = key;
+
+        var td_major = document.createElement("td");
+        td_major.innerHTML = value['major'];
+
+        var td_classes = document.createElement("td");
+        td_classes.innerHTML = value['classes'];
+
+        var td_email = document.createElement("td");
+        td_email.innerHTML = "<a href=\"mailto:" + value['email'] + "\">" + value['email'] + "</a>";
+
+        tr.appendChild(td_name);
+        tr.appendChild(td_major);
+        tr.appendChild(td_classes);
+        tr.appendChild(td_email);
+
+        var category = value['category'];
+
+        if (category.indexOf("Engineering") !== -1) {
+            console.log("eng before: " + document.getElementById("fellows-eng-body-table").childElementCount);
+            document.getElementById("fellows-eng-body-table").appendChild(tr.cloneNode(true));
+            console.log("eng after: " + document.getElementById("fellows-eng-body-table").childElementCount);
+        }
+        if (category.indexOf( "Natural Science") !== -1) {
+            console.log("ns before: " + document.getElementById("fellows-nat-body-table").childElementCount);
+            document.getElementById("fellows-nat-body-table").appendChild(tr.cloneNode(true));
+            console.log("ns after: " + document.getElementById("fellows-nat-body-table").childElementCount);
+        }
+        if (category.indexOf( "Humanities") !== -1) {
+            document.getElementById("fellows-hum-body-table").appendChild(tr.cloneNode(true));
+        }
+
+    });
+});
