b67331e1730dcb0101f98022344d1c49cbc82d2d
Jingwen Hu <jinghuw@gmail.com>
2021-07-07 05:44:04 -0700

    navbar!

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -26,73 +26,90 @@ let categories = {
 }
 
 var navbarContent = `
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark">
-        <a class="navbar-brand" href="home.html">
-            <img src="../resources/images/wrcphoenixlogo.png" width="30" height="30" alt="">
-        </a>
-        <a class="navbar-brand" href="home.html">Will Rice College</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    
+<nav class="navbar fixed-top navbar-expand-lg">
+    <a class="navbar-brand" href="home.html">
+        <img src="../resources/wrc-logos/classic-crest-transparent [png].png" width="50" height="50" alt="">
+    </a>
+    <a class="navbar-brand" href="home.html"><h1 class = "navbar-title">WILL RICE COLLEGE</h1></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle @about" href="http://example.com" data-toggle="dropdown" aria-expanded="false">
-                        About
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="./history.html">History</a>
-                        <a class="dropdown-item" href="./williammarsh.html">William Marsh Rice Jr.</a>
-                        <a class="dropdown-item" href="./traditions.html">Traditions</a>
-                        <a class="dropdown-item" href="beer-bike.html">Beer Bike</a>
-                        <a class="dropdown-item" href="./today.html">Today</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle @people" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        People
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="./a-team.html">A-Team</a>
-                        <a class="dropdown-item" href="./alumni-and-associates.html">Alumni + Associates</a>
-                        <a class="dropdown-item" href="./new-students.html">New Students</a>
-                        <hr>
-                        <a class="dropdown-item" href="./student-leadership.html">Student Leadership</a>
-                        <a class="dropdown-item" href="./paa.html">PAAs</a>
-                        <a class="dropdown-item" href="./fellows.html">Fellows</a>
-                        <a class="dropdown-item" href="./rha.html">RHAs</a>
-                        <a class="dropdown-item" href="./strive.html">STRIVE</a>
-                        <a class="dropdown-item" href="./caregivers.html">Caregivers</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle @operating" href="http://example.com" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Operating
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="./diet.html">Diet</a>
-                        <a class="dropdown-item" href="./constitution-bylaws.html">Constitution/Bylaws</a>
-                        <a class="dropdown-item" href="./housing-and-parking.html">Housing and Parking</a>
-                        <a class="dropdown-item" href="./contact.html">Contact</a>
-                        <a class="dropdown-item" href="./donate.html">Donate</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle @resources" href="http://example.com" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Resources
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="./forms.html">Forms</a>
-                        <a class="dropdown-item" href="./resources.html">Resources</a>
-                        <a class="dropdown-item" href="./calendar.html">Calendar</a>
-                        <a class="dropdown-item" href="./brand-resources.html">Brand Resources</a>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </nav>`;
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="http://example.com" data-toggle="dropdown"
+                    aria-expanded="false">
+                    ABOUT
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="./history.html">History</a>
+                    <a class="dropdown-item" href="./williammarsh.html">William Marsh Rice Jr.</a>
+                    <a class="dropdown-item" href="./traditions.html">Traditions</a>
+                    <a class="dropdown-item" href="beer-bike.html">Beer Bike</a>
+                    <a class="dropdown-item" href="./today.html">Today</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    PEOPLE
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="./a-team.html">A-Team</a>
+                    <a class="dropdown-item" href="./alumni-and-associates.html">Alumni + Associates</a>
+
+                    <hr>
+                    <a class="dropdown-item" href="./student-leadership.html">Student Leadership</a>
+                    <a class="dropdown-item" href="./paa.html">PAAs</a>
+                    <a class="dropdown-item" href="./fellows.html">Fellows</a>
+                    <a class="dropdown-item" href="./rha.html">RHAs</a>
+                    <a class="dropdown-item" href="./strive.html">STRIVE</a>
+                    <a class="dropdown-item" href="./caregivers.html">Caregivers</a>
+
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="http://example.com" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    OPERATING
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="./diet.html">Diet</a>
+                    <a class="dropdown-item" href="./constitution-bylaws.html">Constitution/Bylaws</a>
+                    <a class="dropdown-item" href="./housing-and-parking.html">Housing and Parking</a>
+                    <a class="dropdown-item" href="./contact.html">Contact</a>
+                    <a class="dropdown-item" href="./donate.html">Donate</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="http://example.com" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    RESOURCES
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="./forms.html">Forms</a>
+                    <a class="dropdown-item" href="./finances.html">Finances</a>
+                    <a class="dropdown-item" href="./calendar.html">Calendar</a>
+                    <a class="dropdown-item" href="./brand-resources.html">Brand Resources</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-highlight" href="./new-students.html">
+                    NEW STUDENTS
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-bold" href="https://riceconnect.rice.edu/donation/support-will-rice">
+                    DONATE
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
+`;
 
 let pagename = location.pathname.split("/").slice(-1)[0].replace(".html", "");
 navbarContent = navbarContent.replace(categories[pagename], "active");
