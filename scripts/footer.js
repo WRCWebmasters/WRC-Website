17a16a25ed2b53f174ac4405f6bfa4d3898012b9
Jingwen Hu <jinghuw@gmail.com>
2023-04-24 03:55:14 -0500

    new design yeas

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,33 +1,57 @@
 var footerContent = `
-<footer class="bg-rust">
-      <div style="background-color: rgba(0, 0, 0, 0.2)">
-        <div style="float: left; padding: 0.75rem" class="float-center">
-          <a class="text-light" href="./home.html">Will Rice College 2023</a>
-          <a class="text-light"> | </a>
-          <a class="text-light" target="_blank" href="https://www.rice.edu/"
-            >Rice University</a
-          >
-        </div>
-        <div style="float: right; padding: 0.75rem" class="float-center">
-          <a
-            target="_blank"
-            href="https://docs.google.com/forms/d/e/1FAIpQLSdC1XYoXA8i3sT9gIW09oF5B58rGeDl5qhGfN9NOsuJ23Rkag/viewform?usp=sf_link"
-            class="text-light"
-            >Website Feedback Form | </a
-          >
-          <a class="text-light"
-            >Made possible with
-            <a class="text-dark" target = "_blank" href="https://mdbootstrap.com/"
-              >MDBootstrap.com</a
-            >
-            <a class="text-light"> and </a>
-            <a class = "text-dark" target = "_blank" href="https://www.flaticon.com/">FlatIcon</a></a
-          >
+  <div class="container py-5"  data-wow-delay="0.1s">
+    <div style="display: flex; justify-content: center;">
+      <img class="footer-logo" width="60px" src="../resources/images/wrcphoenixlogo_original.png" />
+    </div>
+    <hr style="  border-top: 3px solid var(--gold); margin: 30px 0" />
+    <div class="row g-5">
+
+      <div class="col-lg-3 col-md-6">
+        <h4 class="text-white mb-4">Quick Links</h4>
+        <a class="btn btn-link" target="_blank"
+          href="https://docs.google.com/forms/d/e/1FAIpQLSeyfsUhSL3uE04xBhw3_rhijYqfCpXU8dCKD3mciANnVf6uzg/viewform">Financial
+          Assistance Form</a>
+        <a class="btn btn-link" href="calendar.html">Calendar</a>
+        <a class="btn btn-link" href="diet.html">Diet</a>
+        <a class="btn btn-link" href="forms.html#space">Space Reservation</a>
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <h4 class="text-rust mb-4">Quick Links</h4>
+        <a class="btn btn-link" target="_blank"
+          href="https://docs.google.com/forms/d/e/1FAIpQLScvmBPMKeOOU3VDeEczDVyWDNWl4jqkYAV84YgvsHg4jf3G6w/viewform">Party
+          Registration</a>
+        <a class="btn btn-link" href="housing-and-parking.html">Housing & Parking</a>
+        <a class="btn btn-link" href="requesting-money.html">Budget Spending Requests</a>
+        <a class="btn btn-link" href="contact.html">Contact</a>
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <h4 class="text-white mb-4">Contact</h4>
+        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>(713) 348 6061</p>
+        <p class="mb-2"><i class="fa fa-envelope me-3"></i>willrice@rice.edu</p>
+        <div class="d-flex pt-2">
+          <a class="btn btn-square btn-outline-light rounded-circle me-2" target="_blank"
+            href="https://www.facebook.com/groups/365957593418249"><i class="fab fa-facebook-f"></i></a>
+          <a class="btn btn-square btn-outline-light rounded-circle me-2" target="_blank"
+            href="https://www.youtube.com/user/willriceelections"><i class="fab fa-youtube"></i></a>
+          <a class="btn btn-square btn-outline-light rounded-circle me-2" target="_blank"
+            href="https://www.instagram.com/willricecollege/"><i class="fab fa-instagram"></i></a>
         </div>
       </div>
-    </footer>
+      <div class="col-lg-3 col-md-6">
+        <h4 class="text-white mb-4">Address</h4>
+        <a style="color: white" href="https://goo.gl/maps/shuZUDRZiTj7qTLu7" target="_blank">
+          <p class="mb-2">6330 Main St</p>
+          <p class="mb-2">Will Rice College</p>
+          <p class="mb-2">Houston, TX 77005</p>
+        </a>
+      </div>
+    </div>
+  </div>
+
+
 `;
 
 let footerElement = document.createElement("div");
+footerElement.className = "container-fluid bg-dark text-light footer mt-5 py-5 wow fadeIn"; 
 footerElement.innerHTML = footerContent;
-document.getElementById("footer-placeholder").appendChild(footerElement);
+document.getElementById("footer-placeholder").replaceWith(footerElement);
--- scripts/navbar.js
+++ scripts/navbar.js
@@ -25,97 +25,73 @@ let categories = {
 };
 
 var navbarContent = `
-<nav class="navbar navbar-light fixed-top navbar-expand-lg">
-    <a class="navbar-brand" href="home.html">
-        <img src="../resources/wrc-logos/classic-crest-transparent [png].png" width="50" height="50" alt="">
-    </a>
-        <a class="navbar-brand flex-grow-1" id = "nav-title" href="home.html"><h1 class = "navbar-title">Will Rice College</h1></a>
-        <a class="navbar-brand flex-grow-1" id="nav-title-short" href="home.html"><h1 class = "navbar-title">WRC</h1></a>
+  <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+    <h1 style="font-size: 2rem" class="m-0">Will Rice College</h1>
+  </a>
+  <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarCollapse">
+    <div class="navbar-nav ms-auto p-4 p-lg-0">
+      <div class="nav-item dropdown">
+        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">ABOUT</a>
+        <div class="dropdown-menu m-0">
+          <a href="history.html" class="dropdown-item">History</a>
+          <a href="traditions.html" class="dropdown-item">Traditions</a>
+          <a href="beer-bike.html" class="dropdown-item">Beer Bike</a>
+        </div>
+      </div>
+      <div class="nav-item dropdown">
+        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">PEOPLE</a>
+        <div class="dropdown-menu m-0">
+          <a href="a-team.html" class="dropdown-item">A-Team</a>
+          <a href="alumni-and-associates.html" class="dropdown-item">Alumni & Associates</a>
+          <a href="student-leadership.html" class="dropdown-item">Student Leadership</a>
+          <a href="paa.html" class="dropdown-item">PAAs</a>
+          <a href="fellows.html" class="dropdown-item">Fellows</a>
+          <a href="rha.html" class="dropdown-item">RHAs</a>
+          <a href="pca.html" class="dropdown-item">PCAs</a>
+          <a href="strive.html" class="dropdown-item">STRIVE</a>
+          <a href="caregivers.html" class="dropdown-item">Caregivers</a>
 
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
 
-    <div class="collapse navbar-collapse flex-row-reverse" id="navbarNavDropdown">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item dropdown">
-                <a class="nav-link" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    ABOUT
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="./history.html">History</a>
-                    <a class="dropdown-item" href="./traditions.html">Traditions</a>
-                    <a class="dropdown-item" href="beer-bike.html">Beer Bike</a>
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link " href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    PEOPLE
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="./a-team.html">A-Team</a>
-                    <a class="dropdown-item" href="./alumni-and-associates.html">Alumni + Associates</a>
+        </div>
+      </div>
+      <div class="nav-item dropdown">
+        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">OPERATING</a>
+        <div class="dropdown-menu m-0">
+          <a href="diet.html" class="dropdown-item">Diet</a>
+          <a href="budget.html" class="dropdown-item">Budget & Spending</a>
+          <a href="constitution-bylaws.html" class="dropdown-item">Constitution & By-laws</a>
+          <a href="contact.html" class="dropdown-item">Contact</a>
+        </div>
+      </div>
+      <div class="nav-item dropdown">
+        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">RESOURCES</a>
+        <div class="dropdown-menu m-0">
+          <a href="feature.html" class="dropdown-item">Communications</a>
+          <a href="quote.html" class="dropdown-item">Forms</a>
+          <a href="team.html" class="dropdown-item">Finances</a>
+          <a href="testimonial.html" class="dropdown-item">Calendar</a>
+          <a href="404.html" class="dropdown-item">Brand Resources</a>
+          <a href="404.html" class="dropdown-item">Rice Mutual Aid</a>
 
-                    <hr>
-                    <a class="dropdown-item" href="./student-leadership.html">Student Leadership</a>
-                    <a class="dropdown-item" href="./paa.html">PAAs</a>
-                    <a class="dropdown-item" href="./fellows.html">Fellows</a>
-                    <a class="dropdown-item" href="./rha.html">RHAs</a>
-                    <a class="dropdown-item" href="./strive.html">STRIVE</a>
-                    <a class="dropdown-item" href="./caregivers.html">Caregivers</a>
+        </div>
+      </div>
+      <a href="contact.html" class="nav-item nav-link">DONATE</a>
+      <a href="new-students.html" class="nav-item nav-link">NEW STUDENTS</a>
 
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link " href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    OPERATING
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="./diet.html">Diet</a>
-                    <a class="dropdown-item" href="./budget.html">Budget and Spending</a>
-                    <a class="dropdown-item" href="./constitution-bylaws.html">Constitution/By-Laws</a>
-                    <a class="dropdown-item" href="./contact.html">Contact</a>
-
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link " href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    RESOURCES
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="./communications.html">Communications</a>
-                    <a class="dropdown-item" href="./forms.html">Forms</a>
-                    <a class="dropdown-item" href="./finances.html">Finances</a>
-                    <a class="dropdown-item" href="./calendar.html">Calendar</a>
-                    <a class="dropdown-item" href="./brand-resources.html">Brand Resources</a>
-                    <a class="dropdown-item" target = "_blank" href="https://linktr.ee/ricemutualaid">Rice Mutual Aid</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link nav-bold" target = "_blank" href="https://riceconnect.rice.edu/donation/support-will-rice">
-                    DONATE
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link nav-highlight" href="./new-students.html">
-                    NEW STUDENTS
-                </a>
-            </li>
-        </ul>
     </div>
-</nav>
-`;
+  </div>
 
-$(function () {
-    $(document).scroll(function () {
-        var $nav = $(".navbar");
-        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height()/2);
-    });
-});
+`;
 
 let pagename = location.pathname.split("/").slice(-1)[0].replace(".html", "");
 navbarContent = navbarContent.replace(categories[pagename], "active");
 
-let navbarElement = document.createElement("div");
+let navbarElement = document.createElement("nav");
+navbarElement.className = "navbar navbar-expand-lg bg-white navbar-light sticky-top p-0";
+
 navbarElement.innerHTML = navbarContent;
-document.getElementById("navbar-placeholder").appendChild(navbarElement);
+document.getElementById("navbar-placeholder").replaceWith(navbarElement);
+
